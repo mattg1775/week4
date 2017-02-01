d53bcbeb40baea136b8f405466ec20d2eda8cae9
Matt Grabowski <m-grabowski@kellogg.northwestern.edu>
2017-01-31 23:11:57 -0600

    Attempt

--- poker/poker.js
+++ poker/poker.js
@@ -32,10 +32,13 @@ window.getDeck = function() {
   $(function() {
     $(".deal").on("click", function(event) {
       event.preventDefault()
-      $("#c1").attr("src", "http://golearntocode.com/images/cards/" + window.getDeck() + ranks[2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'] + suits['clubs', 'diamonds', 'hearts', 'spades'] + cards.push + ".png")
-      $("#c2").attr("src", "http://golearntocode.com/images/cards/" + Array.prototype.shuffle() + window.getDeck() + ".png")
+      $("#c1").attr("src", "http://golearntocode.com/images/cards/" + window.getDeck() + ".png") //I KNOW "getRandomInt" ISN'T RIGHT
+      $("#c2").attr("src", "http://golearntocode.com/images/cards/" + getRandomInt(1,6) + ".png")
       $("#c3").attr("src", "http://golearntocode.com/images/cards/" + getRandomInt(1,6) + ".png")
       $("#c4").attr("src", "http://golearntocode.com/images/cards/" + getRandomInt(1,6) + ".png")
       $("#c5").attr("src", "http://golearntocode.com/images/cards/" + getRandomInt(1,6) + ".png")
+// I DON'T KNOW WHAT CODE GOES ON THE END OF THESE STRINGS IN LINES 35-39 IN ORDER TO SHOW A RANDOM CARD
+// EVERY SYNTAX COMBINATION I TRIED FAILED, AND I ALMOST WENT BLIND READING ONLINE TUTORIALS
+
     })
   })
